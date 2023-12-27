b91c03189f1b6b9f6d85ec26acb9dea82f304190
karimkanafani <karim.kanafani@mail.mcgill.ca>
2023-12-27 20:36:24 +0400

    Intro section done

---
+++ my-portfolio/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import './globals.css'
+import Header from "@/components/header";
+
+const inter = Inter({subsets: ['latin']})
+
+export const metadata: Metadata = {
+    title: 'Karim | Portfolio',
+    description: 'This is the portfolio for Karim Kanafani',
+}
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
+        <div className="bg-[#f4a261] absolute bottom-[-60rem] -z-10 right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem]"></div>
+        <div className="bg-[#264653] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
+        {children}
+        </body>
+        </html>
+    )
+}
