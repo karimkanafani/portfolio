f9899c0b5a5e32d00c66701b702002b595430818
karimkanafani <karim.kanafani@mail.mcgill.ca>
2024-01-11 13:28:45 -0500

    Finished the header section.

--- my-portfolio/src/components/intro.tsx
+++ my-portfolio/src/components/intro.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import Image from "next/image";
 import headshot from "../../public/headshot.png";
 import { motion } from 'framer-motion';
@@ -8,10 +8,16 @@ import Link from "next/link";
 import {BsArrowRight, BsLinkedin} from "react-icons/bs";
 import {HiDownload} from "react-icons/hi";
 import {FaGithubSquare} from "react-icons/fa";
+import {useInView} from "react-intersection-observer";
+import {useActiveSectionContext} from "@/context/active-section-context";
+import {useSectionInView} from "@/lib/hooks";
 
 export default function Intro() {
+
+    const { ref } = useSectionInView("Home", 0.5);
+
     return (
-        <section id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+        <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div
--- my-portfolio/src/components/projects.tsx
+++ my-portfolio/src/components/projects.tsx
@@ -1,11 +1,19 @@
-import React, {useRef} from 'react';
+"use client";
+
+import React, {useEffect, useRef} from 'react';
 import SectionHeading from "@/components/section-heading";
 import {projectsData} from "@/lib/data";
 import Project from "@/components/project";
+import {useInView} from "react-intersection-observer";
+import {useActiveSectionContext} from "@/context/active-section-context";
+import {useSectionInView} from "@/lib/hooks";
 
 export default function Projects() {
+
+  const { ref } = useSectionInView("Projects", 0.5);
+
     return (
-        <section id="projects" className="scroll-mt-28">
+        <section ref={ref} id="projects" className="scroll-mt-28">
             <SectionHeading>My projects</SectionHeading>
             <div>
                 {
