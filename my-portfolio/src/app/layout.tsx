16c35c70d124431623e6ccb34b977c00372dec88
karimkanafani <karim.kanafani@mail.mcgill.ca>
2024-01-11 21:41:30 -0500

    Added footer, updated CV, fixed height issue.

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/header";
 import React from "react";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import {Toaster} from "react-hot-toast";
+import Footer from "@/components/footer";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -20,8 +21,8 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className="!scroll-smooth">
-        <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
-        <div className="bg-[#f4a261] absolute bottom-[-60rem] -z-10 right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]
+        <body className={`${inter.className} bg-gray-50 text-gray-950 h-[3600px] relative pt-28 sm:pt-36`}>
+        <div className="bg-[#f4a261] absolute bottom-[-60rem] -z-10 right-[-11rem] h-[11.25rem] w-[31.25rem] rounded-full blur-[10rem]
         sm:w-[68.75rem]"></div>
         <div className="bg-[#264653] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]
         sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
@@ -29,6 +30,7 @@ export default function RootLayout({
             <Header />
             {children}
             <Toaster position="top-right"/>
+            <Footer />
         </ActiveSectionContextProvider>
         </body>
         </html>
